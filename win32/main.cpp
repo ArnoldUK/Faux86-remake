2996bb364231362c68b6e859c76daa40da7975e9
Arnie <107566722+ArnoldUK@users.noreply.github.com>
2023-10-05 01:13:45 +0100

    Release v1.23 Minor update.

--- win32/main.cpp
+++ win32/main.cpp
@@ -20,6 +20,9 @@
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+
+#define WIN32_LEAN_AND_MEAN
+
 #include <Windows.h>
 #include <stdio.h>
 #include <new>
